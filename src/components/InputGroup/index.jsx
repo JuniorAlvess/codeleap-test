eccc15ff8f8fcf2196bb6138626db2c1d1cc3de3
Junior Alves <junioralves.jav@gmail.com>
2023-10-16 17:48:29 -0300

    feat: added properties

--- src/components/InputGroup/index.jsx
+++ src/components/InputGroup/index.jsx
@@ -1,13 +1,15 @@
 import * as S from './styles';
 
 // eslint-disable-next-line react/prop-types
-const InputGroup = ({ type, label, onChange, placeholder, isRequired }) => {
+const InputGroup = ({ type, defaultValue, value, label, onChange, placeholder, isRequired }) => {
   return (
     <S.Group>
       {label && <S.Label>{label}</S.Label>}
       <S.Input
         type={type || 'text'}
         placeholder={placeholder}
+        defaultValue={defaultValue}
+        value={value}
         required={isRequired}
         onChange={onChange}
       />
--- src/components/TextareaGroup/index.jsx
+++ src/components/TextareaGroup/index.jsx
@@ -1,11 +1,17 @@
 import * as S from './styles';
 
 // eslint-disable-next-line react/prop-types
-const TextareaGroup = ({ label, onChange, placeholder, isRequired }) => {
+const TextareaGroup = ({ label, defaultValue, value, onChange, placeholder, isRequired }) => {
   return (
     <S.Group>
       {label && <S.Label>{label}</S.Label>}
-      <S.Textarea placeholder={placeholder} required={isRequired} onChange={onChange} />
+      <S.Textarea
+        placeholder={placeholder}
+        defaultValue={defaultValue}
+        value={value}
+        required={isRequired}
+        onChange={onChange}
+      />
     </S.Group>
   );
 };
