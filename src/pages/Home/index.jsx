77546881f59ed68d814dcc9c31105ff71d3405e1
Junior Alves <junioralves.jav@gmail.com>
2023-10-16 18:04:31 -0300

    feat: Scroll up implemented

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,21 @@
 import Header from '../../components/Header';
 import Posts from '../../components/Posts';
+
+import { BsArrowUpCircle } from 'react-icons/bs';
+import { useScroll } from '../../hooks/getScroll';
 import * as S from './styles';
 
 const Home = () => {
+  const { scroll } = useScroll();
+
   return (
     <S.Container>
       <Header />
       <Posts />
+      <BsArrowUpCircle
+        className={scroll > 500 ? 'arrow-up enable' : 'arrow-up'}
+        onClick={() => window.scrollTo(0, 0)}
+      />
     </S.Container>
   );
 };
